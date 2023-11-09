f343d9e887e1923b43aa3d79f3560c49a25d4cba
Devesh B Sullivan <deveshbsullivan@Deveshs-MacBook-Pro-2.local>
2023-11-09 15:07:41 -0700

    Init Commit

---
+++ index.py
@@ -0,0 +1,12 @@
+# NBA PLAYER PROJECTION MODEL
+
+#API https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/static/players.md
+
+#career stats
+from nba_api.stats.endpoints import playercareerstats
+
+#live data
+#from nba_api.live.nba.endpoints import scoreboard
+
+
+#TODO: pip install nba_api
